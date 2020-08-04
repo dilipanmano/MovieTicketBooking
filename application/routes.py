dfd805a2d056ded9dab719d812ee933a14e645a3
Dilipan <dilipanm.2016.i@ritchennai.edu.in>
2020-08-04 18:49:56 +0530

    getmethod

--- application/routes.py
+++ application/routes.py
@@ -1,24 +1,32 @@
 from application import app
-from flask import render_template
+from flask import render_template, Response, request
 
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",index=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html",login=True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("register.html",register=True)
 
 @app.route("/movies")
 def movies():
-    return "<h1>Movie list</h1>"
+    moviesData=[{"mid":"1","name": "Pulp Fiction","poster": "static/images/Pulpfiction.jpg"},
+                {"mid":"2","name": "The Shawshank Redemption", "poster": "static/images/Shawshank.jpg"},
+                {"mid":"3","name": "Goodfellas", "poster": "static/images/Goodfellas.jpg"}]
+    return render_template("movies.html",movies=True,moviesData=moviesData)
 
 @app.route("/booking")
 def booking():
-    return "<h1>Booking</h1>"
+    id = request.args.get('movieID')
+    name = request.args.get('mname')
+    poster = request.args.get('mposter')
+    data = {'id':id,'name':name,'poster':poster}
+    return render_template("booking.html",booking=True,data=data)
